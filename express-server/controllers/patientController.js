478d3137abf0bfd1c13f8bf86853fd9f9d76bd2a
liehtt <liehtt@gmail.com>
2020-09-26 18:58:11 +1000

    fix registerPatient api

--- express-server/controllers/patientController.js
+++ express-server/controllers/patientController.js
@@ -5,22 +5,33 @@ function registerPatient(req, res) {
   Patient.find({phone: req.body.phone}, function(err, patient){
     //cannot find the patient, create phone
     if(err){
-      let newPatient = new Patient({
-        phone: req.body.phone,
-        name: req.body.name
-      })
       
-      newPatient.save(function(err, data){
-        if(err){
-          res.send('error-saving');
-        } else {
-          res.send({status: true});
-        }
-      })
     } else{
-      res.send({status: false});
+      console.log(patient);
+      if (patient.length === 0) {
+        let newPatient = new Patient({
+          phone: req.body.phone,
+          name: req.body.name
+        })
+        
+        newPatient.save(function(err, data){
+          if(err){
+            res.send('error-saving');
+          } else {
+            res.send({status: true});
+          }
+        })
+      } else {
+        res.send({status: false});
+      }
     }
   })
+}
+
+function confirmPhoneNumber(req, res) {
+  Patient.find({phone: req.body.phone, SMSpasscode: req.body.SMSpasscode}, function(err, patient) {
+
+  }) 
 }
 
 module.exports = {
