deeaf43ab870ce9f506b90e14b4015d3e26c7929
Jock Harkness <harknessjock@gmail.com>
2020-09-26 14:04:33 +1000

    updated typo

--- express-server/models/patient.js
+++ express-server/models/patient.js
@@ -1,7 +1,7 @@
   import mongoose from 'mongoose';
   const { Schema } = mongoose;
 
-  const PatientSchema = new Schema({
+  const PatientSchema = new mongoose.Schema({
     phone:  {type: Number, required: true, unique: true},
     name: String,
     patient_key: String,
