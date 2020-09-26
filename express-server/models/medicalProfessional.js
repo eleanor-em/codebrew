1c345e7feb7d953a62a1e9930ddb9dc2494fa62a
Jock Harkness <harknessjock@gmail.com>
2020-09-26 14:01:08 +1000

    Added MedicalProfessionalSchema

---
+++ express-server/models/medicalProfessional.js
@@ -0,0 +1,16 @@
+  import mongoose from 'mongoose';
+  const { Schema } = mongoose;
+
+  const MedicalProfessionalSchema = new mongoose.Schema(
+    {
+      phone:  {type: Number, required: true, unique: true},
+      role: {
+        type: String,
+        enum: ['GP', 'pharmacist']
+      },
+      email: String,
+      passwordHash: String
+  }
+);
+const MedicalProfessional = mongoose.model('MedicalProfessional', MedicalProfessionalSchema);
+module.exports = MedicalProfessional;
