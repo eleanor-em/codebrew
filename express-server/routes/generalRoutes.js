e48703ac984f900a804e250568d5ddd7b0f054fc
Jock Harkness <harknessjock@gmail.com>
2020-09-26 20:23:25 +1000

    Update generalRoutes.js

--- express-server/routes/generalRoutes.js
+++ express-server/routes/generalRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const patientController = require('../controllers/patientController');
+const patientController = require('../controllers/medicalProfessionalController');
 
 router.post('/registerPatient', patientController.registerPatient);
 router.post('/confirmPhoneNumber', patientController.confirmPhoneNumber);
 router.post('/getUserPrescriptions', patientController.getUserPrescriptions);
+router.post('/addUser', medicalProfessionalController.addUser);
+
 
 module.exports = router;
