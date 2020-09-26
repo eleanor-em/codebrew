b1f50358721d1b772bd1c306cfa025aaac2f6685
Eleanor McMurtry <elmcmurtry1@gmail.com>
2020-09-26 20:28:33 +1000

    fix copy paste error

--- express-server/routes/generalRoutes.js
+++ express-server/routes/generalRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const patientController = require('../controllers/patientController');
-const patientController = require('../controllers/medicalProfessionalController');
+const medicalProfessionalController = require('../controllers/medicalProfessionalController');
 
 router.post('/registerPatient', patientController.registerPatient);
 router.post('/confirmPhoneNumber', patientController.confirmPhoneNumber);
